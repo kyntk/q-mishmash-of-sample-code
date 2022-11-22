1ec8bfbdf818456142b767e3aaa685f16bf22f83
kyntk <yuto.kaneko.1128@gmail.com>
2022-11-22 11:56:10 +0900

    Implement say method

--- rust-gem-greeting/src/lib.rs
+++ rust-gem-greeting/src/lib.rs
@@ -1,5 +1,5 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+fn say() -> String {
+    format!("Hello, World!")
 }
 
 #[cfg(test)]
@@ -8,7 +8,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = say();
+        assert_eq!(result, "Hello, World!");
     }
 }
